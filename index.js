fed4cb6aefa54e8fdd4df72ab48a77b4e9802ad6
Barbora Novakova <55717978+Barbora838@users.noreply.github.com>
2021-06-24 19:22:35 -0700

    updated sending only position

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-const port = process.env.PORT || 8888
+const port = process.env.PORT || 3333
 var counter = 1
 
 // TODO: add a public page that redirects to the GitHub repository
 // app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello http://localhost:8888/</h1>');
+  res.send('<h1>Hello http://localhost:3333/</h1>');
 });
 
 
 function onConnection(socket) {
   socket.on('skeleton', data => {
-    console.log(counter + " "+ `Message received: ${data}`)
-    counter++
-    socket.broadcast.emit('skeleton', data)  
-    ///will send the message to all the other clients except the newly created connection
+    var parsed = JSON.parse(data);
+    console.log(counter + " " + `Message received: ` + JSON.stringify(parsed.position))
+    console.log(" ")
+    counter = counter + 1
+    //socket.broadcast.emit('chat', data)
+    socket.broadcast.emit('skeleton', JSON.stringify(parsed.position))  
   })
+  setTimeout(function() {
+    socket.send('Sent a message 4seconds after connection!');
+ }, 4000);
 }
 
 io.on('connection', onConnection)
